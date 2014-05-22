3e8f6567d76bdedc8abe1ac569578124cd52d722
Raúl Contreras <rxco.ar@gmail.com>
2014-05-22 15:07:54 -0500

    nueva configuracion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,8 @@ AeconomicoWeb::Application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.action_dispatch.x_sendfile_header = nil
+
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
