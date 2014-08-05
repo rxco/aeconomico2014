1ebdab43ac1964565ea32c967c0d317bc9460169
Raúl Contreras <rxco.ar@gmail.com>
2014-08-05 15:22:49 -0500

    subiendo cambios de JS en admin

--- app/assets/javascripts/init_functions_generic.js
+++ app/assets/javascripts/init_functions_generic.js
@@ -13,7 +13,7 @@ function llamadaAjax(_url, _params,_fnsuccess,_fndone,_fnfail,_fnalways){
 
 /*Se pasa una URL y los parametros necesarios para llamar una acción METHOD GET*/
 function llamadaAjaxGet(_url, _params,_fnsuccess,_fndone,_fnfail,_fnalways){
-
+	console.log(_url);
 		var jqxhr = $.getJSON( _url , _params, _fnsuccess)
 		  .done(_fndone)
 		  .fail(_fnfail)
